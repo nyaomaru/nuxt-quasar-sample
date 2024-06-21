2ee79e2804bdec3f65b345273c7b7f238c189937
nyaomaru <nyaonyao0725@gmail.com>
2024-06-22 08:47:53 +0900

    fix: meta tag setting

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
 <script lang="ts" setup>
 import Header from '@/components/organisms/Header.vue';
 import Footer from '@/components/organisms/Footer.vue';
+
+useHead({
+  title: 'Nyaomaru Nuxt',
+  titleTemplate: productCategory => {
+    return productCategory ? `${productCategory} - Nyaomaru Nuxt` : 'Nyaomaru Nuxt';
+  },
+  meta: [
+    { name: 'description', content: 'Nyaomaru Page' },
+    { name: 'keywords', content: 'Nyaomaru' },
+    { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
+    { property: 'og:title' },
+  ],
+
+  noscript: [{ innerHTML: 'This is content for browsers with no JS (or disabled JS)' }],
+});
 </script>
 
 <template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,9 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
-import { useMeta } from 'quasar';
+const route = useRoute();
 
-// const metaData = {
-//   title: 'Nyaomaru Nuxt',
-
-//   meta: {
-//     description: { name: 'description', content: 'Top Page' },
-//     keywords: { name: 'keywords', content: 'Nyaomaru' },
-//     equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
-//     ogTitle: {
-//       property: 'og:title',
-//     },
-//   },
-
-//   noscript: {
-//     default: 'This is content for browsers with no JS (or disabled JS)',
-//   },
-// };
-
-// useMeta(metaData);
-
-// useMeta(() => {
-//   return {
-//     title: 'Nyaomaru Nuxt',
-//     meta: {
-//       description: { name: 'description', content: 'Top Page' },
-//       keywords: { name: 'keywords', content: 'Nyaomaru' },
-//       equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
-//       ogTitle: {
-//         property: 'og:title',
-//       },
-//     },
-
-//     noscript: {
-//       default: 'This is content for browsers with no JS (or disabled JS)',
-//     },
-//   };
-// });
+useHead({
+  meta: [{ property: 'og:title', content: `App Name - ${route.meta.title}` }],
+});
 </script>
 
 <template>
@@ -46,9 +12,6 @@ import { useMeta } from 'quasar';
       <slot name="header" />
     </header>
     <main class="main">
-      <!-- <q-drawer v-model="rightDrawerOpen" show-if-above side="right" bordered>
-        <div>test</div>
-      </q-drawer> -->
       <q-page-container>
         <slot />
       </q-page-container>
