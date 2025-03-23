04eb3f10f440b1efd54f9c519c33cc7e18251a14
nyaomaru <nyaonyao0725@gmail.com>
2025-03-23 19:41:27 +0900

    chore: favicon setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
   devtools: { enabled: true },
   modules: ['nuxt-quasar-ui', '@nuxt/eslint'],
   eslint: {},
