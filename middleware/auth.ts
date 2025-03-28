f689e4a688c5bf96888fa39125eaefa0b5f78d23
nyaomaru <nyaonyao0725@gmail.com>
2025-03-28 20:03:06 +0900

    fix: auth check middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,21 @@
 import { ROUTE } from '@/constants/route';
 
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   const auth = useAuthState();
+  if (auth.value.isAuthenticated) return;
 
-  if (!auth.value.isAuthenticated) {
-    return navigateTo(ROUTE.LOGIN, { redirectCode: 301 });
+  const { fetchWithAuth } = useFetchWithAuth();
+
+  try {
+    const response = await fetchWithAuth<Response>('/api/auth/refresh', { method: 'POST' });
+    const data = await response.json();
+
+    if (!response.ok) throw new Error('Refresh token invalid');
+
+    auth.value.isAuthenticated = true;
+    auth.value.accessToken = data.access_token;
+  } catch (error) {
+    resetAuth();
+    return navigateTo(ROUTE.LOGIN);
   }
 });
