3c2eb0fed4939f5715b09919f6b413ceef036dc5
nyaomaru <nyaonyao0725@gmail.com>
2025-03-28 20:00:46 +0900

    update: auth check schema

--- schemas/login.ts
+++ schemas/login.ts
@@ -7,8 +7,10 @@ export const authSchema = z.object({
 
 export type AuthSchema = z.infer<typeof authSchema>;
 
-export const authCheckSchema = authSchema.extend({
-  isAuthenticated: z.boolean(),
-});
+export const authCheckSchema = authSchema
+  .extend({
+    isAuthenticated: z.boolean(),
+  })
+  .omit({ password: true });
 
 export type AuthCheckSchema = z.infer<typeof authCheckSchema>;
