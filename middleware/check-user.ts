f025d8e611eec39aaa012c0d20e8fb7d0df7dd0a
nyaomaru <nyaonyao0725@gmail.com>
2025-03-28 20:02:50 +0900

    add: check use middleware

---
+++ middleware/check-user.ts
@@ -0,0 +1,16 @@
+import { ROUTE } from '@/constants/route';
+
+export default defineNuxtRouteMiddleware(async () => {
+  try {
+    const { count } = await $fetch('/api/auth/check', {
+      credentials: 'include',
+    });
+
+    if (count < 1) {
+      return navigateTo(ROUTE.REGISTER);
+    }
+  } catch (error) {
+    console.error('User check failed', error);
+    return navigateTo(ROUTE.REGISTER);
+  }
+});
