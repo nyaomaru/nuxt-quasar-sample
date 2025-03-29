bc426be1f6119b160cd5237f010aa7be1e2c4b4e
nyaomaru <nyaonyao0725@gmail.com>
2025-03-29 21:32:38 +0900

    add: SuccessCard

---
+++ components/atoms/SuccessCard.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+type Props = {
+  successMessage: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  successMessage: '',
+});
+</script>
+
+<template>
+  <q-card v-show="showError" class="bg-secondary text-white q-pa-sm">
+    <q-card-section>
+      <div class="text-h6">{{ successMessage }}</div>
+    </q-card-section>
+  </q-card>
+</template>
